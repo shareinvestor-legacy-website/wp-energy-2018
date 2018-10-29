7679851536b6c7476a510daa78f3c0dac4efcc52
Yuvadee Thongcharoen <yuvadee@shareinvestor.com>
2018-10-29 16:20:41 +0700

    modify stock qoute

--- public/themes/default/views/shortcode/stockquote.blade.php
+++ public/themes/default/views/shortcode/stockquote.blade.php
@@ -19,9 +19,7 @@
                         <div class="stock-change">
                             <p><span>{{t('change')}}</span><span> {{$stock->present()->change}}</span></p>
                             <p><span>{{t('volume.shares')}}</span><span> {{$stock->present()->volume}}</span></p>
-                        </div><div class="stock-change">
-                            <p><span>{{t('change')}} (%)</span><span> {{$stock->present()->percent_change}}%</span></p>
-                            <p><span>{{t('value.baht')}}</span><span> {{$stock->present()->value}}</span></p>
+                        </div><div class="stock-change"><p><span>{{t('change')}} (%)</span><span> {{$stock->present()->percent_change}}%</span></p><p><span>{{t('value.baht')}}</span><span> {{$stock->present()->value}}</span></p>
                         </div>
                     </div>
                 </div>
@@ -32,10 +30,7 @@
                         <div class="stock-change">
                             <p><span>{{t('prior')}} :</span><span>{{$stock->present()->prior}}</span></p>
                             <p><span>{{t('days.range')}}</span><span> {{$stock->present()->range_days}}</span></p>
-                        </div><div class="stock-change">
-                            <p><span>{{t('price.open')}}</span><span> {{$stock->present()->open}}</span></p>
-                            <p><span>{{t('52weeks.range')}}</span><span> {{$stock->present()->range_52weeks}}</span></p>
-
+                        </div><div class="stock-change"><p><span>{{t('price.open')}}</span><span> {{$stock->present()->open}}</span></p><p><span>{{t('52weeks.range')}}</span><span> {{$stock->present()->range_52weeks}}</span></p>
                         </div>
                     </div>
                 </div>
