3b3394713ec40a527f342e9dc6362baf0eb17264
Yuvadee Thongcharoen <yuvadee@shareinvestor.com>
2018-11-06 15:46:52 +0700

    update style seen ir home

--- public/themes/default/views/shortcode/home/stock.blade.php
+++ public/themes/default/views/shortcode/home/stock.blade.php
@@ -5,7 +5,7 @@
         <p class="stock--datetime">{{t('last.updated')}} <time class="d-inline-block">{{$stock->present()->datetime('dd MMM yyyy HH:mm')}}</time></p>
     </div>
     <div class="stock__symbol">
-        <p>{{t('set.symbol')}}</p>
+        <p class="h3">{{t('set.symbol')}}</p>
         <p>{{$stock->present()->symbol}}</p>
     </div>
     <div class="stock__value">
