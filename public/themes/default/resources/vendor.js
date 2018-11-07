e6055440022c36b7f0e447313ee4b15e748ea08b
Thanakorn Tawsan <thanakorn@shareinvestor.com>
2018-11-07 18:02:33 +0700

    reverse version of script

--- public/themes/default/resources/vendor.js
+++ public/themes/default/resources/vendor.js
@@ -1,11 +1,14 @@
-
-//Import scss, css, js files, it can be either dist or src
 import './vendor/jquery-ui/jquery-ui-1.10.4.custom.min.css'
+import './vendor/bootstrap/css/bootstrap.min.css'
 import './vendor/fontawesome/css/all.css'
+import './vendor/owlCarousel/dist/assets/owl.carousel.min.css'
+import './vendor/owlCarousel/dist/assets/owl.theme.default.min.css'
 import './vendor/animate.css'
 import './vendor/magnific-popup/magnific-popup.min.css'
 
 import './vendor/jquery-ui/jquery-ui-1.10.4.custom.min'
+import './vendor/bootstrap/js/bootstrap.min.js'
+import './vendor/owlCarousel/dist/owl.carousel.min.js'
 import './vendor/jquery-viewport-checker/dist/jquery.viewportchecker.min'
 import './vendor/clamp.min.js'
 import './vendor/magnific-popup/magnific-popup.min.js'
--- public/themes/default/views/layouts/component/css.blade.php
+++ public/themes/default/views/layouts/component/css.blade.php
@@ -1,7 +1,4 @@
 
-<link href="//fonts.googleapis.com/css?family=Fira+Sans:300,300i,400,400i,500,500i,600,600i" rel="stylesheet">
-<link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,300i,400,400i,500,500i,600,600i" rel="stylesheet">
 <link rel="stylesheet" href="{{chunkhash_path('/themes/default/assets/vendor.css')}}">
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css">
 <link rel="stylesheet" href="{{chunkhash_path('/themes/default/assets/theme.css')}}">
