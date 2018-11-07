f684b652e1f30d79d8800d75b85bc9e928132461
Thanakorn Tawsan <thanakorn@shareinvestor.com>
2018-11-07 17:47:58 +0700

    use javascript and stylesheet from CDN

--- public/themes/default/views/layouts/component/script.blade.php
+++ public/themes/default/views/layouts/component/script.blade.php
@@ -1,5 +1,9 @@
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script async src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script defer src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script defer src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script defer src="{{chunkhash_path('/themes/default/assets/vendor.js')}}"></script>
+
+<script defer src="//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
+
 <script defer src="{{chunkhash_path('/themes/default/assets/theme.js')}}"></script>
