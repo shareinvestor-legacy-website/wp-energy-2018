b9bf2f550c479b3440f7a70f7125f391e6f84a2b
Thanakorn Tawsan <thanakorn@shareinvestor.com>
2018-11-09 16:04:30 +0700

    update whistlebolwing form

--- public/themes/default/views/shortcode/form/whistleblowing.blade.php
+++ public/themes/default/views/shortcode/form/whistleblowing.blade.php
@@ -27,7 +27,7 @@
         </div>
         <div class="form-group col-sm-6">
             <label for="telephone">{{t('telephone')}}</label>
-            <input class="form-control" name="telephone" type="text" value="{{old('telephone', @$telephone)}}" required>
+            <input class="form-control" name="telephone" type="text" value="{{old('telephone', @$telephone)}}">
 
             @include('component.error-message', ['field' => 'telephone'])
 
