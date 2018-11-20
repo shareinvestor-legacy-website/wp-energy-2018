4f59bc41e47a03a4565fb66d934e1bd72c93ef35
Thanakorn Tawsan <thanakorn@shareinvestor.com>
2018-11-20 09:27:56 +0700

    remove animation script

--- public/themes/default/resources/js/index.js
+++ public/themes/default/resources/js/index.js
@@ -1,7 +1,6 @@
 import './menu';
 import './highcharts';
 import './owl';
-import './viewport';
 import './datepicker';
 import './ellipsis';
 import './popup';
--- public/themes/default/resources/js/viewport.js
+++
@@ -1,9 +0,0 @@
-class viewport {
-    constructor() {
-        if($('[data-vp-add-class]').length) {
-            $('[data-vp-add-class]').viewportChecker();
-        }
-    }
-}
-
-export default new viewport;
