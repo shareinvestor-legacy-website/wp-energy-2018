668e4d8def03473fe078809c379e1af6ed4c46f0
Thanakorn Tawsan <thanakorn@shareinvestor.com>
2018-11-26 18:51:20 +0700

    remove close of tags

--- public/themes/default/views/shortcode/form/whistleblowing.blade.php
+++ public/themes/default/views/shortcode/form/whistleblowing.blade.php
@@ -20,7 +20,7 @@
         </div>
         <div class="form-group col-sm-6">
             <label for="telephone">{{t('telephone')}}</label>
-            <input class="form-control" name="telephone" type="text" value="{{old('telephone', @$telephone)}}" placeholder="Ex. 080-1234567" />>
+            <input class="form-control" name="telephone" type="text" value="{{old('telephone', @$telephone)}}" placeholder="Ex. 080-1234567" />
 
             @include('component.error-message', ['field' => 'telephone'])
 
