f3f0594b0132846b9e1c299305c915d1cfec1384
Thanakorn Tawsan <thanakorn@shareinvestor.com>
2018-11-29 10:31:54 +0700

    update style of hero-banner

--- public/themes/default/views/shortcode/home/hero-banner.blade.php
+++ public/themes/default/views/shortcode/home/hero-banner.blade.php
@@ -5,7 +5,7 @@
         @if($post->present()->isMp4)
 
             <div class="item-video">
-                <video autoplay muted loop width="109%">
+                <video autoplay muted loop height="100%">
                     <source src="{{ $post->present()->file ?? $post->present()->url }}" type="video/mp4">
                 </video>
             </div>
