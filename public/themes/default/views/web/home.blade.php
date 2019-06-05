90516d72c8df5983e8a1e24ddac05d86a9a411c8
Mongkon Wirunputi <mongkon@shareinvestor.com>
2019-06-05 15:38:55 +0700

    merge page home

--- public/themes/default/views/web/home.blade.php
+++ public/themes/default/views/web/home.blade.php
@@ -9,24 +9,24 @@
 
     @component('component.dialog',['dialog' => $dialog])@endcomponent
 
-    {!! $child->present()->body !!}
+    {!! $page->present()->body !!}
 
 @stop
 
 
 @push('style')
+
     {!! isset($page) ? $page->present()->custom_css : '' !!}
-    @foreach($pages as $child)
-        {!! $child->present()->custom_css !!}
-    @endforeach
+
+
+
 @endpush
 
 
 @push('script')
+
     {!! isset($page) ? $page->present()->custom_js : '' !!}
-    @foreach($pages as $child)
-        {!! $child->present()->custom_js !!}
-    @endforeach
+
 @endpush
 
 
