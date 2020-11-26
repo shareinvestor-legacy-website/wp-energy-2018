f2a4dedf79d8638e6b033e26cb2bc8b4769957f6
Thanakorn Tawsan <thanakorn@shareinvestor.com>
2020-11-26 11:46:21 +0700

    update highlight of award

--- public/themes/default/views/web/award/component/highlight.blade.php
+++ public/themes/default/views/web/award/component/highlight.blade.php
@@ -2,9 +2,11 @@
     <div class="row">
         <div class="col-12 col-md-7">
 
-            <h3 class="h2 text-white mb-3">{{$post->present()->alternate_title}}</h3>
+            <h3 class="h2 text-white mb-3">{{$post->present()->title}}</h3>
 
-            <h4>{{$post->present()->title}}</h4>
+            @if ($post->alternate_title)
+                <h4>{{$post->present()->alternate_title}}</h4>
+            @endif
 
             {!! $post->present()->body !!}
 
